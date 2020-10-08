bb5dd12ac7ee2f480a96416df8d6bc7c2dd46035
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-08 09:45:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Search algorithm
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // declares the array
+    int[] numbers = new int[10];
+
+    // gets numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = input.nextInt();
+    }
+    
+    // asks for the number they want to find
+    System.out.println("Please enter a number to find");
+    int findNumber = input.nextInt();
+
+    // find number and tell the user
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] == findNumber) {
+        System.out.println(findNumber + " is located at index " + i);
+      }
+    }
     
   }
 }
